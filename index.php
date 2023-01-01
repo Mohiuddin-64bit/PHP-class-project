e02d00d458c9f76078895c0da4d8f54749990123
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-01-01 14:43:58 +0600

    change service section text

--- index.php
+++ index.php
@@ -109,10 +109,7 @@
         <div class="section-title" data-aos="fade-up">
           <h2>Services</h2>
           <p>
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur
-            ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
-            quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-            Quia fugiat sit in iste officiis commodi quidem hic quas.
+            In our Institute we have several Technology.
           </p>
         </div>
 
